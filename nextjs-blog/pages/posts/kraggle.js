a9e8a1fd0939ca30156cae03baf37f73e81fe6a0
Tim Abell <tim@timwise.co.uk>
2020-12-31 17:50:48 +0000

    back link for kraggle page

--- nextjs-blog/pages/posts/kraggle.js
+++ nextjs-blog/pages/posts/kraggle.js
@@ -1,3 +1,9 @@
+import Link from 'next/link'
 export default function Kraggle() {
-    return <h1>Beware, the kraggle</h1>
+    return (
+        <>
+        <h1>Beware, the kraggle</h1>
+        <Link href="/"><a>home</a></Link>
+        </>
+    )
 }
