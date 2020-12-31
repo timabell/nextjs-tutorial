a1f77c4b4bb010bec30b5fbdabd2604cee036c82
Tim Abell <tim@timwise.co.uk>
2020-12-31 16:48:30 +0000

    some new pages
    https://nextjs.org/learn/basics/navigate-between-pages/pages-in-nextjs

---
+++ nextjs-blog/pages/posts/kraggle.js
@@ -0,0 +1,3 @@
+export default function Kraggle() {
+    return <h1>Beware, the kraggle</h1>
+}
---
+++ nextjs-blog/pages/posts/little-britain.js
@@ -0,0 +1,3 @@
+export default function LittleBritain(){
+    return <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FM%2FMV5BMjEyMjI0MTQ3OF5BMl5BanBnXkFtZTcwNzQ0NzI1MQ%40%40._V1._CR1%2C1%2C372%2C464_UY1200_CR166%2C0%2C630%2C1200_AL_.jpg&f=1&nofb=1"/>
+}
